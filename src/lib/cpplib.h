f1bb07d93c1e471e44d73e032f982b070f1c70ad
Haoxin Ma <haoxinma@usc.edu>
2021-09-26 10:26:18 -0700

    Update cpplib.h

--- src/lib/cpplib.h
+++ src/lib/cpplib.h
@@ -12,14 +12,14 @@ class CPPLib {
  public:
   //q3
   ResultCode VectorCommander(std::vector<int>& v, int left, int right, int find_value, Command command);
-  //q3
+  //q5
   void mergeArray(std::vector<int>& input1, std::vector<int>& input2);
-  //q6
+  //q8
   bool CheckValidExpression(const std::string& a);
 };
 
 
-// q4
+// q6
 class Car_shallow
 {
 public:
